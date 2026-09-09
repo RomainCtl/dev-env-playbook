7b8bfb0d105cf09b2cfe63d54d22bc91ca57c143
RomainCtl <39402602+RomainCtl@users.noreply.github.com>
2026-09-09 17:39:27 +0200

    fix: clean install script

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -e
 
-USERNAME=$(whoami)
-
 printf '\n Provisioning dev environment, Please wait...\n\n'
 
 sudo apt update && sudo apt install -y ansible
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook --connection local --inventory inventory.ini --extra-var USERNAME="${USERNAME}" playbook.yml
+ansible-playbook --connection local --inventory inventory.ini playbook.yml
 
 printf '\n\nYour environment is ready! Don''t forget to reload your prompt to finish installation\n'
