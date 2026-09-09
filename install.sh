2b16ea02eabd26139cef6e9498ea9707dafa73d2
RomainCtl <39402602+RomainCtl@users.noreply.github.com>
2026-09-09 17:38:07 +0200

    fix: brew installation and vi personalization

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ printf '\n Provisioning dev environment, Please wait...\n\n'
 sudo apt update && sudo apt install -y ansible
 
 ansible-galaxy install -r requirements.yml
-ansible-playbook --connection local --inventory invetory.ini --extra-var USERNAME="${USERNAME}" playbook.yml
+ansible-playbook --connection local --inventory inventory.ini --extra-var USERNAME="${USERNAME}" playbook.yml
 
 printf '\n\nYour environment is ready! Don''t forget to reload your prompt to finish installation\n'
