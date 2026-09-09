41063cc36a3b99fb089c830b31b5d905cc8ca0e4
RomainCtl <39402602+RomainCtl@users.noreply.github.com>
2026-09-09 19:30:55 +0200

    fix: ohmyzsh brew completion script

--- roles/shell/files/.oh-my-zsh/custom/brew-completion.zsh
+++ roles/shell/files/.oh-my-zsh/custom/brew-completion.zsh
@@ -1,7 +1,7 @@
 ##see: https://docs.brew.sh/Shell-Completion
 if type brew &>/dev/null
 then
-        FPATH="$(brew --prefix)/share/zsh/site-functions:$(FPATH)"
+        FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
         autoload -Uz compinit
         compinit
